ee8f3fa32ec5e4a521da2fb57d000e4bd7e609a4
Fred Mitchell <fred.mitchell@gmx.de>
2020-03-23 16:34:43 +0100

    will be eliminating the subproject, moving the lexer into the main crate.

--- src/lib.rs
+++ src/lib.rs
@@ -3,30 +3,40 @@
 use std::collections::HashMap;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use std::result::Result;
+use std::fs;
+use std::path::Path;
+
+use lrlex::lrlex_mod;
+use lrpar::lrpar_mod;
+
+
+lrlex_mod!("imapdsl.l");
+lrpar_mod!("imapdsl.y");
 
 #[derive(Hash, Eq, PartialEq, Debug)]
-enum AuthType {
+pub enum AuthType {
     Login,
     Plain,
 }
 
 #[derive(Hash, Eq, PartialEq, Debug)]
-enum Login {
+pub enum Login {
     NamePassword((String, String)),
 }
 
 #[derive(Hash, Eq, PartialEq, Debug)]
-enum Server {
+pub enum Server {
     URL((String, u32)), // URI and port
 }
 
 /// Key for the struct
-trait Key {
+pub trait Key {
     fn key(&self) -> String;
 }
     
 #[derive(Hash, Eq, PartialEq, Debug)]
-struct Account {
+pub struct Account {
     name: String,
     login: Login,
     server: Server,
@@ -39,7 +49,7 @@ impl Key for Account {
 }
 
 #[derive(Hash, Eq, PartialEq, Debug)]
-struct Filter {
+pub struct Filter {
     name: String,
 }
 
@@ -47,9 +57,53 @@ impl Key for Filter {
     fn key(&self) -> String { self.name.clone() }
 }
 
-struct ImapFilterOperation {
+pub struct ImapFilterOperation {
     accounts: HashMap<String, Account>,
     filters: HashMap<String, Filter>
 }
 
+impl ImapFilterOperation {
+    fn new<P: AsRef<Path>>(path: P) -> Result<Self, String> {
+        let mut imf = ImapFilterOperation{
+            accounts: HashMap::new(),
+            filters: HashMap::new()
+        };
+        imf.load_configuration(path)
+    }
 
+    /// TODO: Currently this will panic if the config file does not
+    /// TODO: exist. Handle this properly!!!!
+    fn load_configuration<P: AsRef<Path>>(mut self, path: P) -> Result<Self, String> {
+        let lexerdef = imapdsl_l::lexerdef();
+        let emf = fs::read_to_string(path).unwrap();
+
+        let lexer = lexerdef.lexer(&emf);
+        let (res, errs) = imapdsl_y::parse(&lexer);
+        for e in errs {
+            println!("{}", e.pp(&lexer, &imapdsl_y::token_epp));
+        }
+
+        Ok(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// This test depends on a usable sample.emf to be located in the example directory.
+    /// It will panic otherwise, as it should.
+    ///
+    /// Also, this will break on a Windows system, as they have their slashes around
+    /// the wrong way. :p
+    #[test]
+    fn test_lexing() {
+        let sample_file = Path::new(file!())
+            .parent()
+            .unwrap()
+            .join("../example/sample.emf");
+        println!("defined in file: {:?}", sample_file);
+        
+        let ifo = ImapFilterOperation::new(sample_file).unwrap();
+    }
+}
