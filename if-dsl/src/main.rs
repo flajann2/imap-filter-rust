8c733b5f5c889a74accd12497e85e44dde3655d5
Fred Mitchell <fred.mitchell@gmx.de>
2020-03-23 10:32:12 +0100

    deep dive into the understanding of how grmtools works

--- if-dsl/src/main.rs
+++ if-dsl/src/main.rs
@@ -18,20 +18,20 @@ fn main() {
         io::stdout().flush().ok();
         match stdin.lock().lines().next() {
             Some(Ok(ref l)) => {
-                if l.trim().is_empty() {
-                    continue;
-                }
-                // Now we create a lexer with the `lexer` method with which
-                // we can lex an input.
-                let lexer = lexerdef.lexer(l);
-                // Pass the lexer to the parser and lex and parse the input.
-                let (res, errs) = imapdsl_y::parse(&lexer);
-                for e in errs {
-                    println!("{}", e.pp(&lexer, &imapdsl_y::token_epp));
-                }
-                match res {
-                    Some(Ok(r)) => println!("Result: {}", r),
-                    _ => eprintln!("Unable to evaluate expression.")
+                if !l.trim().is_empty() {
+                    // Now we create a lexer with the `lexer` method with which
+                    // we can lex an input.
+                    let lexer = lexerdef.lexer(l);
+                    // Pass the lexer to the parser and lex and parse the input.
+                    let (res, errs) = imapdsl_y::parse(&lexer);
+                    for e in errs {
+                        println!("{}", e.pp(&lexer, &imapdsl_y::token_epp));
+                    }
+                
+                    match res {
+                        Some(Ok(r)) => println!("Result: {}", r),
+                        _ => eprintln!("Unable to evaluate expression.")
+                    }
                 }
             }
             _ => break
