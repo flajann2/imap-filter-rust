dc16aaf5956546f4abae5aa480c9374ac82e2629
Fred Mitchell <fred.mitchell@gmx.de>
2020-05-17 20:54:16 +0200

    secrets exporting properly

--- .fish/functions/set_secrets.fish
+++ .fish/functions/set_secrets.fish
@@ -1,4 +1,4 @@
 function set_secrets -d "just for testing"
-    set -g GMX_EMAIL "fred@notarealemailaddress.com"
-    set -g GMX_PASS "123456_worst_password_ever"
+    set -x GMX_EMAIL "fred@notarealemailaddress.com"
+    set -x GMX_PASS "123456_worst_password_ever"
 end
